347274745fea95eb890417477680b095c6d7c1c2
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2025-06-20 17:11:37 +0200

    projects responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AboutMe from './pages/AboutMe';
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutMe />} />
+      
     </Routes>
   );
 };
